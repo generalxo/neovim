3a489a53cc286d85020cf19be6c7a50990315ead
oliver wasner <oli.wasner@hotmail.com>
2025-04-30 07:34:24 +0200

    php formatter and plugin update

--- lua/config/after/conform.lua
+++ lua/config/after/conform.lua
@@ -2,6 +2,7 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		blade = { "blade-formatter", "rusywind" },
+		php = { "pint" },
 	},
 	formatters = {
 		--	["blade-formatter"] = {},
--- lua/config/after/lsp.lua
+++ lua/config/after/lsp.lua
@@ -1,10 +1,18 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
+local lspconfig = require("lspconfig")
 
 local capabilities = require("blink.cmp").get_lsp_capabilities()
-require("lspconfig").lua_ls.setup({ capabilities = capabilities })
-require("lspconfig").emmet_ls.setup({
+lspconfig.lua_ls.setup({ capabilities = capabilities })
+
+lspconfig.emmet_ls.setup({
+	capabilities = capabilities,
 	filetypes = { "html", "blade" },
+})
+
+lspconfig.tailwindcss.setup({
+	capabilities = capabilities,
+	filetypes = { "html", "blade", "php" },
 })
 
 vim.diagnostic.config({ virtual_text = true })
